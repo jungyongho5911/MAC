2bf83061133c1f717e24931a68ab04ab371d3e00
1027 <1027@hb1009>
2018-04-16 19:14:51 +0900

    2018-04-16-2

--- src/main/webapp/resources/js/app.js
+++ src/main/webapp/resources/js/app.js
@@ -3,12 +3,54 @@ app = (()=>{
 	var init = x =>{
 	    $.getScript(x+'/resources/js/router.js',()=>{
 	        $.extend(new Router(x));
-	        app.home.onCreate();
+	        app.nav.onCreate();
 	    })
 	};
 	return {init:init};
 })();
-app.home=(()=>{
+app.nav=(()=>{
+	var $wrapper,context,view,image;
+	var onCreate=()=>{
+		 $wrapper = $('#wrapper');
+		 $content = $('#content');
+		 context = $.context();
+		 image = $.image();
+	     view = $.javascript()+'/view.js';
+	     setContentView();
+	 };
+	 var setContentView=()=>{
+			 $.getScript(view,()=>{
+				 $(createDiv({
+	    			 id : 'container',
+	    			 clazz : ''
+	    		 })).appendTo($wrapper);
+				 $(createNav({
+					 id:'navbar-default',
+					 clazz:'navbar navbar-default'
+				 })).appendTo('#container');
+				 $(createDiv({
+	    			 id : 'div-container',
+	    			 clazz : 'container-fluid'
+	    		 })).appendTo('#navbar-default');
+				 $(createDiv({
+	    			 id : 'div-navbar-header',
+	    			 clazz : 'navbar-header'
+	    		 })).appendTo('#div-container');
+				 $('#div-navbar-header')
+				 .append('<button id="btn-nav" type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1">');
+				 $(createATag({
+		             id:'a-item',
+		             val:createSpan({
+		                 id:'span-item',
+		                 clazz:'glyphicon-map-marker',
+		                 val:'신제품'})}))
+		        .appendTo($('#btn-nav'));
+				 
+	         });
+		 };
+		 return{onCreate:onCreate}
+	 })();
+app.login=(()=>{
 	var $wrapper,context,view,image;
 	var onCreate=()=>{
 		 $wrapper = $('#wrapper');
@@ -29,19 +71,56 @@ app.home=(()=>{
     			 id : 'div-header',
     			 clazz : 'wrap-tnb-menu'
     		 })).attr('style','background:black; height:60px;').appendTo('#div-main');
-			 $(createPtag({
-				 id:'p-nav',
-				 clazz:'',
-				 val:'M·A·C SELECT 공식몰 첫 구매 시 15% 할인'
-			 })).attr('style','margin-left:600px; color:white; padding:15px;').appendTo('#div-header');
     		 $(createDiv({
     			 id : 'div-second',
     			 clazz : ''
-    		 })).attr('style','background:black; height:60px;').appendTo('#div-header');
+    		 })).attr('style','background:black; height:60px; padding:15px;').appendTo('#div-header');
     		 $(createATag({
     			 id:'a-main',
-    			 val:'MAC'
-    		 })).attr('style','color:white;font-size: 20px;').appendTo('#div-second');
+    			 val:'M·A·C'
+    		 })).attr('style','color:white;font-size: 20px; margin-left:100px;').appendTo('#div-second');
+    		 $(createATag({
+    			 id:'a-new-item',
+    			 val:'신제품'
+    		 })).attr('style','color:white;font-size: 20px; padding:10px;').appendTo('#div-second');
+    		 $(createATag({
+    			 id:'a-item',
+    			 val:'제품'
+    		 })).attr('style','color:white;font-size: 20px; padding:10px;').appendTo('#div-second');
+    		 $(createATag({
+    			 id:'a-best',
+    			 val:'베스트셀러'
+    		 })).attr('style','color:white;font-size: 20px; padding:10px;').appendTo('#div-second');
+    		 $(createATag({
+    			 id:'a-goodbye',
+    			 val:'굿바이즈'
+    		 })).attr('style','color:white;font-size: 20px; padding:10px;').appendTo('#div-second');
+    		 $(createATag({
+    			 id:'a-brand',
+    			 val:'브랜드'
+    		 })).attr('style','color:white;font-size: 20px; padding:10px;').appendTo('#div-second');
+    		 $(createATag({
+    			 id:'a-select',
+    			 val:'맥셀렉트'
+    		 })).attr('style','color:white;font-size: 20px; padding:10px;').appendTo('#div-second');
+    		 $(createATag({
+    			 id:'a-find-store',
+    			 val:'매장안내'
+    		 })).attr('style','color:white;font-size: 20px; padding:10px;').appendTo('#div-second');	
+    		 $(createSpan({
+    			 id:'span-search',
+    			 clazz:'glyphicon glyphicon-search',
+    			 val:''
+    		 })).attr('style','color:white;font-size: 20px; padding:10px; margin-left:300px;').appendTo('#div-second');
+    		 $(createSpan({
+    			 id:'span-email',
+    			 clazz:'glyphicon glyphicon-envelope',
+    			 val:''
+    		 })).attr('style','color:white;font-size: 20px; padding:10px;').appendTo('#div-second');
+    		 $(createATag({
+    			 id:'a-login',
+    			 val:'로그인'
+    		 })).attr('style','color:white;font-size: 20px; padding:10px;').appendTo('#div-second');
          });
 	 };
 	 var content=()=>{
@@ -53,15 +132,15 @@ app.home=(()=>{
 			 $(createTab({
 				 id:'tab-login',
 				 clazz:'table-condensed'
-			 })).attr('style','width:90%; margin: 0auto; margin-top:70px;').appendTo('#mac-store');
+			 })).attr('style','width:90%; margin: 0auto; margin-top:70px; height:70%').appendTo('#mac-store');
 			 $(createHTag({
 				 num : '2',
 				 val : '회원 로그인'
-			 })).attr('style','margin-left: 20px;').appendTo('#tab-login');
+			 })).attr('style','margin-left: 200px; font-size: 4rem; font-weight: normal; line-height:0.875; ').appendTo('#tab-login');
 			 $(createHTag({
 				 num : '',
 				 val : '아직 맥 공식 온라인몰 회원이 아니신가요?'
-			 })).attr('style','margin-left: 20px;').appendTo('#tab-login');
+			 })).attr('style','margin-left: 200px; font-size: 15px;').appendTo('#tab-login');
 			 $(createUL({
 				 id:'ul-login',
 				 clazz:''
@@ -70,27 +149,45 @@ app.home=(()=>{
 				 id:'li-login',
 				 clazz:'',
 				 val:'이메일 주소를 입력해 주세요.'
-			 })).appendTo('#ul-login');
+			 })).attr('style','margin-left: 160px; font-size: 15px;').appendTo('#ul-login');
 			 $('#li-login').append('</br>');
 			 $(createLI({
 				 id:'li-pass',
 				 clazz:'',
 				 val:'비밀번호를 입력해 주세요.'
-			 })).appendTo('#li-login');
+			 })).attr('style','margin-left: 160px; font-size: 15px;').appendTo('#li-login');
 			 $(createDiv({
-					id : 'login-bar',
-					clazz : 'login-bar'
+					id : 'div-input-bar',
+					clazz : 'div-input-bar'
 				})).appendTo('#tab-login');
 			 $(createInput({
 				 id : 'input-login',
 				 val : '*예시:username@network.co.kr',
 				 type :'text'
-			 })).attr('style','height:60px; width:300px; margin-left:20px; border: 1px solid red;').appendTo('#login-bar');
+			 })).attr('style','height:60px; width:300px; margin-left:200px; border: 1px solid red;').appendTo('#div-input-bar');
 			 $(createInput({
 					id : 'input-pass',
 					 val : '*비밀번호(영문 대소문자, 숫자, 특수문자 혼합 8자 이상 조합)',
 					 type :'text'
-				})).attr('style','height:60px; width:300px; margin-left: 20px; border: 1px solid red;').appendTo('#login-bar');
+				})).attr('style','height:60px; width:300px; margin-left: 20px; border: 1px solid red;').appendTo('#div-input-bar');
+			 $('#div-input-bar').append($(createDiv({
+					id : 'div-pass-question',
+					clazz : 'div-input-bar'
+				})));
+			 $(createATag({
+				 id:'a-pass',
+				 val:'비밀번호를 잊어버리셨나요?'			
+			 })).attr('style','margin-left:550px;').appendTo('#div-pass-question');
+			 $('#div-pass-question').append($(createDiv({
+					id : 'div-login-box',
+					clazz : 'div-login-box'
+				})));
+			 $(createButton({
+					id : 'input-login-box',
+					 clazz : 'btn-login',
+					 val :'로그인'
+				})).attr('style','border-radius: 0; height:60px; width:300px; margin-left:200px; background: black; color: white;').appendTo('#div-login-box');
+			 
 		 });
 	 }
 		 return{onCreate:onCreate,content:content}
--- src/main/webapp/resources/js/view.js
+++ src/main/webapp/resources/js/view.js
@@ -1,3 +1,20 @@
+var createNavgation=x=>{
+	return+'<div class="btn-group">'
+	  +'<button type="button" class="btn btn-danger">Action</button>'
+	  +'<button type="button" class="btn btn-danger dropdown-toggle" data-toggle="dropdown" aria-expanded="false">'
+	    +'<span class="caret"></span>'
+	    +'<span class="sr-only">Toggle Dropdown</span>'
+	  +'</button>'
+	  +'<ul class="dropdown-menu" role="menu">'
+	    +'<li><a href="#">Action</a></li>'
+	    +'<li><a href="#">Another action</a></li>'
+	    +'<li><a href="#">Something else here</a></li>'
+	    +'<li class="divider"></li>'
+	    +'<li><a href="#">Separated link</a></li>'
+	  +'</ul>'
+	+'</div>';
+}
+
 var createNav=x=>{
     return'<nav id="'+x.id+'" class="'+x.clazz+'"></nav>';
 }
@@ -9,7 +26,6 @@ var createATag=x=>{
 }
 var createSpan=x=>{
     return '<span id="'+x.id+'" class="glyphicon '+x.clazz+'" aria-hidden="true"> &nbsp;'
-    
     +x.val+'</span>'
 }
 var createHTag=x=>{
@@ -42,4 +58,10 @@ var createForm=x=>{
 
 var createPtag=x=>{
 	return '<p class="'+x.clazz+'" id="'+x.id+'">'+x.val+'</p>';
+}
+
+var createEmail=()=>{
+	return '<div class="site-email-signup-v1 site-email-signup">'
+	+'<a id="site-email-signup" href="/customer_service/contact_us.tmpl" title="이메일 문의하기" class="site-email-signup__link jquery-once-1-processed"><i class="icon--envelope site-email-signup__link-icon"></i></a>'
+	+'</div>';
 }
